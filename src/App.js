3460f19672873fad7cce52ad1eaeaade2904c92f
Carlos <capz2003@gmail.com>
2021-11-26 16:24:52 -0500

    game pick number

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
+
 function App() {
+  let randomNumber = Math.floor(Math.random() * (10 - 1 + 1) * 1);
+  //states
+  const [random, setRandom] = useState(0);
+  const [value, setValue] = useState(null);
+  const [tries, setTries] = useState(0);
+  const [games, setGames] = useState(0);
+  const [isActive, setActive] = useState("false");
+
+  //functions
+
+  const startGame = () => {
+    setRandom(randomNumber);
+    setActive(!isActive);
+  };
+
+  const userGuess = (e) => {
+    setValue(e.target.value);
+  };
+  const compareNumbers = (e) => {
+    e.preventDefault();
+    if (!value || value > 10) window.confirm("Please pick a number between 1-10");
+    if (random !== Number(value)) {
+      window.confirm("Vuelve a intentar");
+      setTries(tries + 1);
+    } else {
+      window.confirm("Number is Correct you win!");
+      setTries(tries + 1);
+      setGames(games + 1);
+      setRandom(randomNumber);
+    }
+    setValue('')
+  };
+  const resetGame = () => {
+    setRandom(randomNumber);
+    setTries(0);
+    setGames(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <div className="container">
+        <h1 className="gametitle">Guess a Number Game</h1>
+        <div className={isActive ? "gamer" : null}>
+          <section className="gamescore">
+            <h2>Tries: {tries}</h2>
+            <h2>Games: {games}</h2>
+          </section>
+          <form className="form" onSubmit={compareNumbers} action="">
+            <label> Guess a Number from 1-10 hit Enter</label>
+            <input onChange={userGuess} type="number" value={value} />
+          </form>
+          <button className="btn-reset" onClick={resetGame}>
+            Reset Game
+          </button>
+        </div>
+        <button
+          className={!isActive ? "btn gamer " : 'btn'} 
+          onClick={() => startGame()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Lets Play
+        </button>
+      </div>
+    </React.Fragment>
   );
 }
 
